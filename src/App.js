316b880f1e7662d93adfba048659ae62fadcd639
Marcos Ulloa <marcosdavidulloa@gmail.com>
2020-10-28 15:03:43 -0300

    Resumen component created

--- src/App.js
+++ src/App.js
@@ -2,13 +2,14 @@ import React, { Component } from 'react';
 import './App.css';
 import Layout from './components/Layout/Layout';
 import NodosSistema from './components/NodosSistema/NodosSistema';
+import Resumen from './components/Resumen/Resumen';
 
 class App extends Component {
   render () {
     return (
       <Layout>
         <NodosSistema />
-        <h1>Resumen (component)</h1>
+        <Resumen /> 
         <h1>Indicadores (component)</h1>
       </Layout>
     );
---
+++ src/components/Resumen/Resumen.js
@@ -0,0 +1,22 @@
+import React from 'react';
+
+const Resumen = () => {
+  return (
+    <div>
+      <div>
+        <h2>Comm RF</h2>
+        <p>DUMMY estado de RF</p>
+      </div>
+      <div>
+        <h2>Estado de alarma</h2>
+        <p>DUMMY (bien, mal, critico)</p>
+      </div>
+      <div>
+        <h2>Fecha de ultimo evento</h2>
+        <p>DUMMY add date via props (how?)</p>
+      </div>
+    </div>
+  );
+};
+
+export default Resumen;
