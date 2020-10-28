5e6232653b0fb3ef702f82ef5f76614b26fbd669
Marcos Ulloa <marcosdavidulloa@gmail.com>
2020-10-28 15:10:38 -0300

    Indicadores component created

--- src/App.js
+++ src/App.js
@@ -3,6 +3,7 @@ import './App.css';
 import Layout from './components/Layout/Layout';
 import NodosSistema from './components/NodosSistema/NodosSistema';
 import Resumen from './components/Resumen/Resumen';
+import Indicadores from './components/Indicadores/Indicadores';
 
 class App extends Component {
   render () {
@@ -10,7 +11,7 @@ class App extends Component {
       <Layout>
         <NodosSistema />
         <Resumen /> 
-        <h1>Indicadores (component)</h1>
+        <Indicadores /> 
       </Layout>
     );
   }
---
+++ src/components/Indicadores/Indicadores.js
@@ -0,0 +1,14 @@
+import React from 'react';
+
+const Indicadores = (props) => {
+  return (
+    <div>
+      <h1>Indicadores</h1>
+      <div>Grafica 1</div>
+      <div>Grafica 2</div>
+    </div>
+
+  );
+};
+
+export default Indicadores;
