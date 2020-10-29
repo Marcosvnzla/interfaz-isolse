cc023fe4f5d7c0465eab401ac3305049f99a8338
Marcos Ulloa <marcosdavidulloa@gmail.com>
2020-10-29 15:13:21 -0300

    changed favicon and logos

--- src/components/NodosSistema/NodosSistema.js
+++ src/components/NodosSistema/NodosSistema.js
@@ -1,11 +1,12 @@
 import React, { Fragment } from 'react';
+import styles from './NodosSistema.module.css';
 const VALOR_DISP_SECUNDARIO_1 = "someString";
 const VALOR_DISP_SECUNDARIO_2 = "someString2";
 
 const NodosSistema = () => {
   return (
     <Fragment>
-      <h2>Nodos del sistema</h2>
+      <h2 className={styles.Title}>Nodos del sistema</h2>
       <select>
         <option>Sin dispositivos conectados</option>
         <option value={VALOR_DISP_SECUNDARIO_1}>1 dispositivo secundario</option>
