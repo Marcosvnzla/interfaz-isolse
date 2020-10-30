b141141692e84eb2b74d42df6a654f6f0f00cc16
Marcos Ulloa <marcosdavidulloa@gmail.com>
2020-10-30 12:33:17 -0300

    added styling and func to Indicadores component

--- src/App.js
+++ src/App.js
@@ -7,32 +7,51 @@ import Indicadores from './components/Indicadores/Indicadores';
 
 class App extends Component {
   state = {
-    RFState: 'todo ok',
-    alarmState: 'todo bien',
+    RFState: 'normal',
+    alarmState: 'normal',
     lastLectureDate: 'no date yet'
   }
 
-  eventMonitor = () => {
+  getEventTime = () => {
     const timeStamp = new Date();
     const date = timeStamp.toDateString();
-    console.log(date);
-    this.setState({
-      RFState: 'todo mal',
-      alarmState: 'se funde',
-      lastLectureDate: date
-    });
-  } 
+    this.setState({lastLectureDate: date});
+  }
+
+  setAlarmState = (lecture) => {
+    this.setState({alarmState: lecture});
+    this.getEventTime();
+  }
+
+  setRFCommonState = (lecture) => {
+    this.setState({RFState: lecture});
+    this.getEventTime();
+  }
+
+  normalState = () => {
+    this.setAlarmState('normal');
+  }
+
+  failureState = () => {
+    this.setAlarmState('failure');
+  }
+  
+  criticalState = () => {
+    this.setAlarmState('critical');
+  }
 
   render () {
     return (
       <Layout>
         <NodosSistema />
-        <Indicadores /> 
+        <Indicadores sdaState={this.state.alarmState} rfCommState={this.state.RFState} /> 
         <Resumen 
           date={this.state.lastLectureDate}
           alarmState={this.state.alarmState}
           rfState={this.state.RFState}/> 
-        <button onClick={this.eventMonitor}>toggle state</button>
+        <button onClick={this.normalState}>toggle normal</button>
+        <button onClick={this.failureState}>toggle failure</button>
+        <button onClick={this.criticalState}>toggle critical</button>
       </Layout>
     );
   }
--- src/components/Indicadores/Indicadores.js
+++ src/components/Indicadores/Indicadores.js
@@ -2,19 +2,54 @@ import React from 'react';
 import styles from './Indicadores.module.css';
 
 const Indicadores = (props) => {
+  let sdaTextMessage = '';
+
+  switch (props.sdaState) {
+    case 'normal':
+      sdaTextMessage = 'normal';
+      break;
+    case 'failure':
+      sdaTextMessage = 'de falla';
+      break;
+    case 'critical':
+      sdaTextMessage = 'critico!';
+      break;
+    default:
+      sdaTextMessage = 'no se pudo obtener informacion del estado';
+      break;
+  }
+
+  let rfTextMessage = '';
+  switch (props.rfCommState) {
+    case 'normal':
+      rfTextMessage = 'normal';
+      break;
+    case 'failure':
+      rfTextMessage = 'de falla';
+      break;
+    case 'critical':
+      rfTextMessage = 'critico!';
+      break;
+    default:
+      rfTextMessage = 'no se pudo obtener informacion del estado';
+      break;
+  }
+
+
+
   return (
     <div className={styles.Indicadores}>
       <h1>Indicadores</h1>
       <div className={styles.ContainerGraficas}>
         <div className={styles.Grafica}>
-          <h2>Indicator 1</h2>
-          <div></div>
-          <p>Legend 1</p>
+          <h2>SDA!</h2>
+          <div className={styles[props.sdaState]}></div>
+          <p>Componente en estado <strong>{sdaTextMessage}</strong></p>
         </div>
         <div className={styles.Grafica}>
-          <h2>Indicator 2</h2>
-          <div></div>
-          <p>Legend 2</p>
+          <h2>RF Common</h2>
+          <div className={styles[props.rfCommState]}></div>
+          <p>Componente en estado <strong>{rfTextMessage}</strong> </p>
         </div>
       </div>
     </div>
