8fa2094f9456ff4c6c6bca07a4a8b76262ea5e58
Marcos Ulloa <marcosdavidulloa@gmail.com>
2020-11-02 14:10:03 -0300

    rendered toggle button

--- src/UI/ToggleBtn/ToggleBtn.js
+++ src/UI/ToggleBtn/ToggleBtn.js
@@ -1,14 +1,15 @@
 import React from 'react';
 import styles from './ToggleBtn.module.css';
+import arrowUp from '../../assets/images/arrow-up.svg';
 
 const ToggleBtn = () => {
   return (
     <div className={styles.ToggleBtn}>
       <div>
-        <img alt=""></img>
+        <img src={arrowUp} alt="arrow"></img>
       </div>
       <div>
-        <img alt=""></img>
+        <img className={styles.rotated} src={arrowUp} alt="arrow"></img>
       </div>
     </div>
   );
--- src/components/Layout/Layout.js
+++ src/components/Layout/Layout.js
@@ -1,11 +1,12 @@
 import React, { Fragment } from 'react';
-
 import Toolbar from '../../UI/Toolbar/Toolbar';
+import ToggleBtn from '../../UI/ToggleBtn/ToggleBtn';
 
 const Layout = (props) => (
   <Fragment>
     <Toolbar />
     {props.children}
+    <ToggleBtn />
   </Fragment>
 );
 
